3967640ecae21e05530618f0603d65f2d58a5719
Tea Hyeung, Son <53033449+thsvkd@users.noreply.github.com>
2020-11-17 01:35:36 +0900

    upload calculator example

---
+++ 20201108a_Calculator/script.js
@@ -0,0 +1,31 @@
+var formulaInput = document.getElementById("formula-input");
+var calcHistDiv = document.getElementById("calc-history");
+
+formulaInput.addEventListener("keyup", function(e) {
+    if (e.keyCode == 13)
+        calculate();
+})
+
+function calculate () {
+
+    var fm = formulaInput.value;
+    var formulaRegex = /^\d+(.\d+)?[+\-*/]{1}\d+(.\d+)?$/;
+    var formulaValid = formulaRegex.test(fm);
+
+    var resultText = "No"
+    
+    if (formulaValid) {
+        var answer;
+        eval('answer=' + fm);
+        resultText = fm + " = ";
+        resultText
+            += (answer % 1 > 0 ? answer.toFixed(2) : answer.toString());
+    }
+    var resultDiv = document.createElement("DIV");
+    resultDiv.appendChild(document.createTextNode(resultText));
+    if (!formulaValid)
+        resultDiv.classList.add("invalid");
+    calcHistDiv.insertBefore(resultDiv, calcHistDiv.firstChild);
+
+    formulaInput.value = "";
+}
